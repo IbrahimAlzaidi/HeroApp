a497cacf51561033cfc6985d5649f4a7499bb28c
Amnah Ali <amnah1li@gmail.com>
2021-10-06 11:42:56 +0300

    add navigation

--- app/src/main/java/com/example/heroapp/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/heroapp/ui/fragment/SearchFragment.kt
@@ -1,17 +1,17 @@
 package com.example.heroapp.ui.fragment
 
 import android.view.LayoutInflater
-import com.example.heroapp.databinding.FragmentMainBinding
+import com.example.heroapp.databinding.FragmentSearchBinding
 import com.example.heroapp.ui.base.BaseFragment
 
 
-class SerachFragment : BaseFragment<FragmentMainBinding>() {
+class SearchFragment : BaseFragment<FragmentSearchBinding>() {
 
     override val LOG_TAG: String
         get() = "Main Fragment"
 
-    override val bindingInflater: (LayoutInflater) -> FragmentMainBinding =
-        FragmentMainBinding::inflate
+    override val bindingInflater: (LayoutInflater) -> FragmentSearchBinding =
+        FragmentSearchBinding::inflate
 
     override fun setup() {}
     override fun addCallBack() {}
