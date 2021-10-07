5eda64f78776f1a66b2735937c9e48993fefda36
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-10-07 03:57:00 +0300

    create adapter

--- app/src/main/java/com/example/heroapp/ui/fragment/base/BaseAdapter.kt
+++ app/src/main/java/com/example/heroapp/ui/fragment/base/BaseAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.heroapp.ui.fragment.base
 
-class BaseAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.heroapp.R
+import com.example.heroapp.databinding.ItemHeroBinding
+import com.example.heroapp.model.response.heroImageModel.HeroImage
+
+class BaseAdapter(private val heroListData: HeroImage) : RecyclerView.Adapter<BaseAdapter.HeroViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_hero, parent, false)
+        return HeroViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
+        holder.binding.apply {
+            heroName.text = heroListData.name
+            Log.i("recyclerView",heroListData.url.toString())
+            Glide.with(heroImage).load(heroListData.url.toString()).into(heroImage)
+        }
+    }
+    override fun getItemCount() = heroListData.id!!.count()
+
+
+    class HeroViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+        val binding = ItemHeroBinding.bind(viewItem)
+
+    }
 }
--- app/src/main/java/com/example/heroapp/ui/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/example/heroapp/ui/fragment/search/SearchFragment.kt
@@ -2,17 +2,27 @@ package com.example.heroapp.ui.fragment.search
 
 import android.view.LayoutInflater
 import com.example.heroapp.databinding.FragmentSearchBinding
+import com.example.heroapp.model.response.heroImageModel.HeroImage
+import com.example.heroapp.ui.fragment.base.BaseAdapter
 import com.example.heroapp.ui.fragment.base.BaseFragment
 
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
-
     override val LOG_TAG: String
         get() = "Main Fragment"
 
     override val bindingInflater: (LayoutInflater) -> FragmentSearchBinding =
         FragmentSearchBinding::inflate
 
-    override fun setup() {}
+    override fun setup() {
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+       //TODO("set data to recyclerView")
+    }
+
+
     override fun addCallBack() {}
+
 }
