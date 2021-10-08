407e04f776846921fcaad5f442de00b3d5c6fabb
Nooralden <nooraldenakel@gmail.com>
2021-10-08 05:59:48 +0300

    modify the Recycler Adapter and add function to onBind_view_Holder to apply the first sold principle, add animation to CardsView in Recycler, add InterFace to hold the Data ant tran it when the Card View clicked

--- app/src/main/java/com/example/heroapp/ui/fragment/base/BaseAdapter.kt
+++ app/src/main/java/com/example/heroapp/ui/fragment/base/BaseAdapter.kt
@@ -1,29 +1,47 @@
 package com.example.heroapp.ui.fragment.base
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.heroapp.R
 import com.example.heroapp.databinding.ItemHeroBinding
-import com.example.heroapp.model.response.heroImageModel.HeroImage
+import com.example.heroapp.model.response.heroModel.Heros
+import com.example.heroapp.model.response.heroModel.Result
+import com.example.heroapp.util.CardViewListener
 
-class BaseAdapter(private val heroListData: HeroImage) : RecyclerView.Adapter<BaseAdapter.HeroViewHolder>() {
+class BaseAdapter(private val heroListData: Heros, private val listener: CardViewListener) :
+    RecyclerView.Adapter<BaseAdapter.HeroViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_hero, parent, false)
         return HeroViewHolder(view)
     }
+
     override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
-        holder.binding.apply {
-            heroName.text = heroListData.name
-            Log.i("recyclerView",heroListData.url.toString())
-            Glide.with(heroImage).load(heroListData.url.toString()).into(heroImage)
+        when (holder) {
+            is HeroViewHolder -> bindData(holder, heroListData.results?.get(position))
         }
     }
-    override fun getItemCount() = heroListData.id!!.count()
+
+    private fun bindData(holder: HeroViewHolder, hero: Result?) {
+        holder.binding.apply {
+            heroName.text = hero?.name
+            Glide.with(heroImage).load(hero?.image?.url).into(heroImage)
+            setRecyclerAnimation(holder)
+            myCardView.apply {
+                startAnimation(setRecyclerAnimation(holder))
+                setOnClickListener { listener.onCardClicked(hero) }
+            }
+        }
+    }
+
+    private fun setRecyclerAnimation(holder: HeroViewHolder) =
+        AnimationUtils.loadAnimation(holder.itemView.context, android.R.anim.slide_in_left)
+
+    override fun getItemCount() = heroListData.results!!.count()
 
 
     class HeroViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
